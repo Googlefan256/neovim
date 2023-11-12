9079857bddeb8d34149d45b03cb0793e9ad132e2
Googlefan <private@googlefan.net>
2023-11-12 16:26:20 +0900

    feat: genetic updates

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,48 +1,50 @@
 local function lspconfig()
-	vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-	vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+  vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+  vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-	-- Use LspAttach autocommand to only map the following keys
-	-- after the language server attaches to the current buffer
-	vim.api.nvim_create_autocmd("LspAttach", {
-		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-		callback = function(ev)
-			local opts = { buffer = ev.buf }
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-			vim.keymap.set("n", "<space>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts)
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, opts) end, })
+  -- Use LspAttach autocommand to only map the following keys
+  -- after the language server attaches to the current buffer
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+      local opts = { buffer = ev.buf }
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+      vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+      vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+      vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, opts)
+      vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<space>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
+    end,
+  })
 end
 
 local function config()
-	require("mason").setup()
-	require("mason-lspconfig").setup_handlers({
-		function(server)
-			local opt = {
-				capabilities = require("cmp_nvim_lsp").default_capabilities(
-					vim.lsp.protocol.make_client_capabilities()
-				),
-			}
-			require("lspconfig")[server].setup(opt)
-		end,
-	})
-	require("lspconfig.ui.windows").default_options.border = "single"
-	lspconfig()
+  require("mason").setup()
+  require("mason-lspconfig").setup_handlers({
+    function(server)
+      local opt = {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(
+          vim.lsp.protocol.make_client_capabilities()
+        ),
+      }
+      require("lspconfig")[server].setup(opt)
+    end,
+  })
+  require("lspconfig.ui.windows").default_options.border = "single"
+  lspconfig()
   require("mason-lspconfig").setup({
     ensecure_installed = {
       "pyright",
@@ -51,15 +53,18 @@ local function config()
       "lua-ls",
       "stylua",
       "gopls",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
     }
   })
 end
 
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	config = config,
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = config,
 }
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -11,7 +11,7 @@ local function config()
 	})
 	local builtin = require("telescope.builtin")
 	vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-	vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+	-- vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
 	vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 end
