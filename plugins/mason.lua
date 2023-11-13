5f7dfa191443c5c9b316676e5eee205050cc51ff
Googlefan <private@googlefan.net>
2023-11-13 22:52:02 +0900

    fix: nls config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -32,32 +32,62 @@ local function lspconfig()
 end
 
 local function config()
-  require("mason").setup()
+  require("mason").setup({
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  })
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "pyright",
+      "eslint",
+      "tsserver",
+      "lua_ls",
+      "gopls",
+      "html",
+      "cssls",
+      "tailwindcss",
+      "denols",
+      "biome",
+    },
+  })
   require("mason-lspconfig").setup_handlers({
     function(server)
+      local nvim_lsp = require("lspconfig")
       local opt = {
         capabilities = require("cmp_nvim_lsp").default_capabilities(
           vim.lsp.protocol.make_client_capabilities()
         ),
       }
-      require("lspconfig")[server].setup(opt)
+      if server == "tsserver" then
+        opt.root_dir = nvim_lsp.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
+      elseif server == "eslint-lsp" then
+        opt.root_dir = nvim_lsp.util.root_pattern(".eslintrc.js", ".eslintrc.json", ".eslintrc")
+      elseif server == "denols" then
+        opt.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
+        opt.init_options = {
+          lint = true,
+          unstable = true,
+          suggest = {
+            imports = {
+              hosts = {
+                ["https://deno.land"] = true,
+                ["https://cdn.nest.land"] = true,
+                ["https://crux.land"] = true,
+              },
+            },
+          },
+        }
+      end
+      nvim_lsp[server].setup(opt)
     end,
   })
   require("lspconfig.ui.windows").default_options.border = "single"
   lspconfig()
-  require("mason-lspconfig").setup({
-    ensecure_installed = {
-      "pyright",
-      "eslint",
-      "tsserver",
-      "lua-ls",
-      "stylua",
-      "gopls",
-      "html-lsp",
-      "css-lsp",
-      "prettier",
-    }
-  })
 end
 
 return {
---
+++ plugins/null-ls.lua
@@ -0,0 +1,33 @@
+local function config()
+	local nls = require("null-ls")
+	nls.setup({
+		sources = {
+			nls.builtins.formatting.stylua,
+			nls.builtins.formatting.gofmt,
+			nls.builtins.formatting.rustfmt,
+			nls.builtins.formatting.black,
+			nls.builtins.formatting.deno_fmt.with({
+				condition = function(utils)
+					return utils.has_file({ "deno.json", "deno.jsonc" })
+				end,
+			}),
+			nls.builtins.formatting.prettier.with({
+				condition = function(utils)
+					return utils.has_file({ ".prettierrc", ".prettierrc.js" })
+				end,
+				prefer_local = "node_modules/.bin",
+			}),
+			nls.builtins.formatting.shfmt,
+			nls.builtins.formatting.biome.with({
+        condition = function(utils)
+          return utils.has_file({ "biome.json" })
+        end,
+      }),
+		},
+	})
+end
+
+return {
+	"nvimtools/none-ls.nvim",
+	config = config,
+}
